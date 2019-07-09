aafd17ec375cb89f3b8e8a921ea51ead3ea0d302
vitek-karas <vitek.karas@microsoft.com>
2019-07-09 13:25:13 -0700

    Cleanup of names

--- core/interop/pinvoke/marshalling/src/MarshallingSample/Int32Marshalling.cs
+++ core/interop/pinvoke/marshalling/src/MarshallingSample/Int32Marshalling.cs
@@ -6,58 +6,69 @@ namespace MarshallingSample
     class Int32Marshalling
     {
         [DllImport(nameof(MarshallingSampleNative))]
-        private static extern void Marshal_Int32_ByValIn([In] int value);
+        private static extern void Marshal_Int32_ByValue_In([In] int value);
 
         [DllImport(nameof(MarshallingSampleNative))]
-        private static extern void Marshal_Int32_ByValOut([Out] int value);
+        private static extern void Marshal_Int32_ByValue_Out([Out] int value);
 
         [DllImport(nameof(MarshallingSampleNative))]
-        private static extern void Marshal_Int32_ByValInOut([In, Out] int value);
+        private static extern void Marshal_Int32_ByValue_InOut([In, Out] int value);
 
         [DllImport(nameof(MarshallingSampleNative))]
-        private static extern void Marshal_Int32_ByRefIn([In] ref int value);
+        private static extern void Marshal_Int32_ByRef_In([In] ref int value);
 
         [DllImport(nameof(MarshallingSampleNative))]
-        private static extern void Marshal_Int32_ByRefOut(out int value);
+        private static extern void Marshal_Int32_ByRef_Out(out int value);
 
         [DllImport(nameof(MarshallingSampleNative))]
-        private static extern void Marshal_Int32_ByRefInOut(ref int value);
+        private static extern void Marshal_Int32_ByRef_InOut(ref int value);
+
+        [DllImport(nameof(MarshallingSampleNative))]
+        private static extern int Marshal_Int32_Ret(int value);
 
         public static void Run()
         {
             Console.WriteLine("----- Int32 marshalling samples -----");
 
+            const int initialValue = 7;
             int value;
 
-            Console.Write("Int32_ByValIn in: ");
-            value = 7;
-            Marshal_Int32_ByValIn(value);
+            Console.WriteLine($"Starting value is always {initialValue}");
+
+            Console.Write("Int32 ByValue In ");
+            value = initialValue;
+            Marshal_Int32_ByValue_In(value);
             Console.WriteLine();
 
-            Console.Write("Int32_ByValOut ");
-            value = 7;
-            Marshal_Int32_ByValOut(value);
+            Console.Write("Int32 ByValue Out ");
+            value = initialValue;
+            Marshal_Int32_ByValue_Out(value);
             Console.WriteLine($"out: {value}");
 
-            Console.Write("Int32_ByValInOut in: ");
-            value = 7;
-            Marshal_Int32_ByValInOut(value);
+            Console.Write("Int32 ByValue InOut ");
+            value = initialValue;
+            Marshal_Int32_ByValue_InOut(value);
             Console.WriteLine($" out: {value}");
 
-            Console.Write("Int32_ByRefIn in: ");
-            value = 7;
-            Marshal_Int32_ByRefIn(ref value);
+            Console.Write("Int32 ByRef In ");
+            value = initialValue;
+            Marshal_Int32_ByRef_In(ref value);
             Console.WriteLine();
 
-            Console.Write("Int32_ByRefOut ");
-            value = 7;
-            Marshal_Int32_ByRefOut(out value);
+            Console.Write("Int32 ByRef Out ");
+            value = initialValue;
+            Marshal_Int32_ByRef_Out(out value);
             Console.WriteLine($"out: {value}");
 
-            Console.Write("Int32_ByRefInOut in: ");
-            value = 7;
-            Marshal_Int32_ByRefInOut(ref value);
+            Console.Write("Int32 ByRef InOut "); 
+            value = initialValue;
+            Marshal_Int32_ByRef_InOut(ref value);
             Console.WriteLine($" out: {value}");
+
+            Console.Write("Int32 Ret ");
+            value = initialValue;
+            Marshal_Int32_Ret(value);
+            Console.WriteLine($" ret: {value}");
         }
     }
 }
--- core/interop/pinvoke/marshalling/src/MarshallingSampleNative/MarshallingSampleNative.cpp
+++ core/interop/pinvoke/marshalling/src/MarshallingSampleNative/MarshallingSampleNative.cpp
@@ -1,34 +1,40 @@
 #include <stdio.h>
 #include "common.h"
 
-extern "C" DLL_EXPORT void STDMETHODCALLTYPE Marshal_Int32_ByValIn(/*[In]*/ int value)
+extern "C" DLL_EXPORT void STDMETHODCALLTYPE Marshal_Int32_ByValue_In(/*[In]*/ int value)
 {
-    printf("%u", value);
+    printf("in: %u", value);
 }
 
-extern "C" DLL_EXPORT void STDMETHODCALLTYPE Marshal_Int32_ByValOut(/*[Out]*/ int value)
+extern "C" DLL_EXPORT void STDMETHODCALLTYPE Marshal_Int32_ByValue_Out(/*[Out]*/ int value)
 {
     value++;
 }
 
-extern "C" DLL_EXPORT void STDMETHODCALLTYPE Marshal_Int32_ByValInOut(/*[In,Out]*/ int value)
+extern "C" DLL_EXPORT void STDMETHODCALLTYPE Marshal_Int32_ByValue_InOut(/*[In,Out]*/ int value)
 {
-    printf("%u", value);
+    printf("in: %u", value);
     value++;
 }
 
-extern "C" DLL_EXPORT void STDMETHODCALLTYPE Marshal_Int32_ByRefIn(/*[In]*/ int * pValue)
+extern "C" DLL_EXPORT void STDMETHODCALLTYPE Marshal_Int32_ByRef_In(/*[In]*/ int * pValue)
 {
-    printf("%u", *pValue);
+    printf("in: %u", *pValue);
 }
 
-extern "C" DLL_EXPORT void STDMETHODCALLTYPE Marshal_Int32_ByRefOut(/*[Out]*/ int * pValue)
+extern "C" DLL_EXPORT void STDMETHODCALLTYPE Marshal_Int32_ByRef_Out(/*[Out]*/ int * pValue)
 {
     (*pValue)++;
 }
 
-extern "C" DLL_EXPORT void STDMETHODCALLTYPE Marshal_Int32_ByRefInOut(/*[In,Out]*/ int * pValue)
+extern "C" DLL_EXPORT void STDMETHODCALLTYPE Marshal_Int32_ByRef_InOut(/*[In,Out]*/ int * pValue)
 {
-    printf("%u", *pValue);
+    printf("in: %u", *pValue);
     (*pValue)++;
+}
+
+extern "C" DLL_EXPORT int STDMETHODCALLTYPE Marshal_Int32_Ret(int value)
+{
+    printf("in: %u", value);
+    return value;
 }
